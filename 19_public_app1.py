d6e60c392b6e9f55143e5c672d66988ead3ed94a
randomguy3367 <simpsons9148@gmail.com>
2024-10-08 14:19:52 +0900

    Update 19_public_app1.py

--- 19_public_app1.py
+++ 19_public_app1.py
@@ -1,26 +1,9 @@
 import streamlit as st
 import pandas as pd
-import plotly.express  as px
-
+import plotly.express as px
 from time import time
 
-# [TODO] 아래 summary를 참고하여 막대 그래프와 라인 그래프를 그리는 함수를 구성해봅니다.
-# """_summary_
-# fig = px.bar(bar_df, 
-#                 title="",
-#                 y='',
-#                 color="",
-#                 hover_data='')
 
-# fig = px.line(line_df,x="일시",y="",
-#             title="",
-#             color='',
-#             line_group='',
-#             hover_name="")
-# """
-
-## *geo는 클릭이 된 지역구들이 들어있는 튜플이라고 생각하시면 됩니다.
-# 여기서 *는 가변인자를 받을때 사용하는 것이다.(데이터 개수, 길이가 막 달라질 수 있는 경우)
 @st.cache_data
 def bar_chart(*geo):
     bar_df = edited_df[edited_df['선택']]
@@ -67,10 +50,3 @@ with col2:
          st.plotly_chart(line_chart(*select))
     t2 = time()
     st.write(t2-t1)
-
-
-# 체크 표시된 지역구만 뽑아서 리스트로 저장합니다.
-
-# 오른쪽 column의 내용을 채워줍니다. (Tab 두개를 만들어줍니다. )
-
-# streamlit run 19_public_app1.py
